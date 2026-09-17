8826ba5631a662904db951402a4a386cddb01f8c
amira <amira@gmail.com>
2026-09-17 03:13:35 +0200

    admin crud done

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,10 @@
+
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    hours = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
